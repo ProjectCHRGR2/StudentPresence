41061afbcf1a9017857bf8d3e5d7789623435d71
TheDenizen <25197071+TheDenizen@users.noreply.github.com>
2019-09-30 11:20:04 +0200

    Camera FAB

--- Android_StudentPresence/app/src/main/java/com/example/studentpresence/MainActivity.java
+++ Android_StudentPresence/app/src/main/java/com/example/studentpresence/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.studentpresence;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -25,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
             }
         });
     }
