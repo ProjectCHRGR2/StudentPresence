3724326ba9ab84a2fbc4ed80842dd2091a4a166e
Delano Brouwer <Delano.Brouwer@live.nl>
2019-11-16 23:35:53 +0100

    Registration System
    Users can now safely register an account. This account can then be managed via Firebase.

--- Android_StudentPresence/app/src/main/java/com/example/studentpresence/MainActivity.java
+++ Android_StudentPresence/app/src/main/java/com/example/studentpresence/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.studentpresence;
 import android.content.Intent;
 import android.os.Bundle;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button loginbutton = findViewById(R.id.login_button);
@@ -23,5 +22,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button registerbutton = findViewById(R.id.register_redirect_button);
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
