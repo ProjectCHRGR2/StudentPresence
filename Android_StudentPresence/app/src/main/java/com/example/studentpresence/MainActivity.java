aefb903a1aba65112d1c2bbdfc948b1217c843d2
Delano Brouwer <Delano.Brouwer@live.nl>
2019-11-21 21:16:00 +0100

    Login Functionality + Fixes
    - Added the ability to log in after creating an account.
    - Fixed NullPointer crash.
    - Fixed 0 Length Password / Email crash.

--- Android_StudentPresence/app/src/main/java/com/example/studentpresence/MainActivity.java
+++ Android_StudentPresence/app/src/main/java/com/example/studentpresence/MainActivity.java
@@ -3,26 +3,66 @@ package com.example.studentpresence;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
+    EditText email;
+    EditText password;
+    Button login_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button loginbutton = findViewById(R.id.login_button);
-        loginbutton.setOnClickListener(new View.OnClickListener() {
+
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.input_email);
+        password = findViewById(R.id.input_password);
+        login_button = findViewById(R.id.login_button);
+
+        //Login
+        login_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, qractivity.class);
-                startActivity(intent);
-            }
+                // Sign in using the Firebase Method "signInWithEmailAndPassword after turning the Email and Password into strings.
+                if (email.getText().length() > 0 && password.getText().length() > 0) {
+                    mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) { // If Sign In is successful, show confirmation to user and go to scanner screen.
+                                Toast.makeText(MainActivity.this, "Login successful.", Toast.LENGTH_LONG).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent intent = new Intent(MainActivity.this, qractivity.class);
+                                startActivity(intent);
+                            } else { //If Sign In fails, show to user. No action.
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+
+                        } //End of onComplete
+                    });
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please fill in both fields.", Toast.LENGTH_SHORT).show();
+                }
+            } // End of onClick
         });
 
+        //Register
         Button registerbutton = findViewById(R.id.register_redirect_button);
         registerbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -31,7 +71,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
 }
--- Android_StudentPresence/app/src/main/java/com/example/studentpresence/RegisterActivity.java
+++ Android_StudentPresence/app/src/main/java/com/example/studentpresence/RegisterActivity.java
@@ -17,7 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText email;
+    EditText email_reg;
     EditText password1;
     EditText password2;
     Button register;
@@ -29,7 +29,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        email = findViewById(R.id.input_email_reg);
+        email_reg = findViewById(R.id.input_email_reg);
         password1 = findViewById(R.id.input_password_reg1);
         password2 = findViewById(R.id.input_password_reg2);
         register = findViewById(R.id.register_button);
@@ -39,22 +39,26 @@ public class RegisterActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() { // On click function initiating the account registration.
             @Override
             public void onClick(View view) {
-                if (password1.getText().toString().equals(password2.getText().toString())) { // Check if Passwords match.
-                    firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), password1.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) { // Call a firebase function to create an account.
-                            if (task.isSuccessful()){
-                                Toast.makeText(RegisterActivity.this, " Successfully Registered.", Toast.LENGTH_LONG).show();
+                if (email_reg.getText().length() > 0 && password1.getText().length() > 0 && password2.getText().length() > 0) { // Length Checker
+                    if (password1.getText().toString().equals(password2.getText().toString())) { // Check if Passwords match.
+                        firebaseAuth.createUserWithEmailAndPassword(email_reg.getText().toString(), password1.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) { // Call a firebase function to create an account.
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegisterActivity.this, " Successfully Registered.", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show(); // Exception message will be returned by Firebase in case of an error.
+                                }
                             }
-                            else {
-                                Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show(); // Exception message will be returned by Firebase in case of an error.
-                            }
-                        }
-                    });
+                        });
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Passwords do not match.", Toast.LENGTH_LONG).show();
+                    }
                 }
                 else {
-                    Toast.makeText(RegisterActivity.this, "Passwords do not match.", Toast.LENGTH_LONG).show();
-            }}
+                    Toast.makeText(RegisterActivity.this, "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 
