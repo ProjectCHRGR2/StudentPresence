8673a957a07f2528325a2cf4ce6d2468c308c49d
Delano Brouwer <Delano.Brouwer@live.nl>
2019-12-01 22:05:33 +0100

    Registration and Login Finish
    When registration is complete, some user info will be sent to the Firebase Database so that it can later on be retrieved and sent to the website database.

--- Android_StudentPresence/app/src/main/java/com/example/studentpresence/MainActivity.java
+++ Android_StudentPresence/app/src/main/java/com/example/studentpresence/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +15,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
     EditText email;
     EditText password;
     Button login_button;
+
+    String checkNum;
+    String checkFirst;
+    String checkLast;
+    String savedEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +38,21 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.input_password);
         login_button = findViewById(R.id.login_button);
 
-        //Login
         login_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Sign in using the Firebase Method "signInWithEmailAndPassword after turning the Email and Password into strings.
                 if (email.getText().length() > 0 && password.getText().length() > 0) {
                     mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
+                        @Override // Sign in with Firebase method.
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) { // If Sign In is successful, show confirmation to user and go to scanner screen.
                                 Toast.makeText(MainActivity.this, "Login successful.", Toast.LENGTH_LONG).show();
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                Intent intent = new Intent(MainActivity.this, qractivity.class);
-                                startActivity(intent);
-                            } else { //If Sign In fails, show to user. No action.
-                                Toast.makeText(MainActivity.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-
-                        } //End of onComplete
+                                Intent intent = new Intent(MainActivity.this,qractivity.class);startActivity(intent);
+                            } else { Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show(); } //If Sign In fails, show to user. No action.
+                        }
                     });
                 }
-                else {
-                    Toast.makeText(MainActivity.this, "Please fill in both fields.", Toast.LENGTH_SHORT).show();
-                }
+                else { Toast.makeText(MainActivity.this, "Please fill in both fields.", Toast.LENGTH_SHORT).show(); }
             } // End of onClick
         });
 
