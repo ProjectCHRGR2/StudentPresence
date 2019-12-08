4841ed5d81b3d521972bc4cf4cfa8200cf1ba271
Delano Brouwer <Delano.Brouwer@live.nl>
2019-12-08 13:59:31 +0100

    Removed Unused Imports
    Removed imports mentioned in the previous review.

--- Android_StudentPresence/app/src/main/java/com/example/studentpresence/RegisterActivity.java
+++ Android_StudentPresence/app/src/main/java/com/example/studentpresence/RegisterActivity.java
@@ -2,7 +2,6 @@ package com.example.studentpresence;
 
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,9 +22,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.annotations.NotNull;
 
-import java.lang.reflect.Array;
 
 public class RegisterActivity extends AppCompatActivity {
 
