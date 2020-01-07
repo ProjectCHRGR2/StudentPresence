c84c71e6a1b37b40e0585d1867d633c3e1748652
Delano Brouwer <Delano.Brouwer@live.nl>
2020-01-07 16:33:32 +0100

    Duplicate Student Number
    Added a check that prevents the registration of duplicate student numbers.

--- Android_StudentPresence/app/src/main/java/com/example/studentpresence/RegisterActivity.java
+++ Android_StudentPresence/app/src/main/java/com/example/studentpresence/RegisterActivity.java
@@ -24,6 +24,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -40,6 +42,7 @@ public class RegisterActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
     String[] classes;
     String tempClasses;
+    public String numChecker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +58,7 @@ public class RegisterActivity extends AppCompatActivity {
         register = findViewById(R.id.register_button);
         dropDownMenu = findViewById(R.id.drop_down_menu);
         userClass = "";
+        numChecker = "1";
 
         mAuth = mAuth.getInstance();
 
@@ -97,30 +101,51 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (email_reg.getText().length() > 0 && password1.getText().length() > 0 && password2.getText().length() > 0 && studentNum.getText().length() > 0 && firstName.getText().length() > 0 && lastName.getText().length() > 0) { // Length Checker to see if every field has been filled in.
                     if (password1.getText().toString().equals(password2.getText().toString())) { // Check if Passwords match.
-                        mAuth.createUserWithEmailAndPassword(email_reg.getText().toString(), password1.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) { // Call a firebase function to create an account.
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(RegisterActivity.this, " Successfully Registered.", Toast.LENGTH_LONG).show();
-                                    FirebaseDatabase database = FirebaseDatabase.getInstance(); // If the Firebase function runs succesfully, enter following values into database.
-                                    DatabaseReference myRef = database.getReference().child("Students").child(mAuth.getUid());
-                                    String student_id = studentNum.getText().toString();
-                                    //Prepare all values for sending below.
-                                    UserProfile User = new UserProfile();
-                                    User.setUserEmail(email_reg.getText().toString());
-                                    User.setUserStudentNum(studentNum.getText().toString());
-                                    User.setUserLastName(lastName.getText().toString());
-                                    User.setUserFirstName(firstName.getText().toString());
 
-                                    User.setUserClass(userClass);
-                                    myRef.setValue(User); // Send values to Database.
-                                    Intent intent = new Intent(RegisterActivity.this, TakeSelfie.class);
-                                    intent.putExtra("StudentId",student_id);
-                                    startActivity(intent);
-                                  
-                                } else {Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();} // Exception message will be returned by Firebase in case of an error.
-                            }
-                        });
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        final DatabaseReference databaseRef = database.getReference(); // Set reference to the database.
+                        final DatabaseReference studentRef = databaseRef.child("Students"); // Set reference to the child in the database called "Classes".
+                        String studentNumString = studentNum.getText().toString();
+                        studentRef.orderByChild("userStudentNum").equalTo(studentNumString).addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    System.out.println("LOADED" + dataSnapshot.getValue());
+                                    try{numChecker = dataSnapshot.getValue().toString();}
+                                    catch(Exception NullPointerException){numChecker = "1";}
+                                    System.out.println("-=-=-=- SNAPSHOT CONTAINS: " + numChecker);
+                                    if (numChecker.equals("1") || numChecker == null) {
+                                        mAuth.createUserWithEmailAndPassword(email_reg.getText().toString(), password1.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) { // Call a firebase function to create an account.
+                                                if (task.isSuccessful()) {
+                                                    Toast.makeText(RegisterActivity.this, " Successfully Registered.", Toast.LENGTH_LONG).show();
+                                                    FirebaseDatabase database = FirebaseDatabase.getInstance(); // If the Firebase function runs succesfully, enter following values into database.
+                                                    DatabaseReference myRef = database.getReference().child("Students").child(mAuth.getUid());
+                                                    String student_id = studentNum.getText().toString();
+                                                    //Prepare all values for sending below.
+                                                    UserProfile User = new UserProfile();
+                                                    User.setUserEmail(email_reg.getText().toString());
+                                                    User.setUserStudentNum(studentNum.getText().toString());
+                                                    User.setUserLastName(lastName.getText().toString());
+                                                    User.setUserFirstName(firstName.getText().toString());
+
+                                                    User.setUserClass(userClass);
+                                                    myRef.setValue(User); // Send values to Database.
+                                                    Intent intent = new Intent(RegisterActivity.this, TakeSelfie.class);
+                                                    intent.putExtra("StudentId",student_id);
+                                                    startActivity(intent);
+
+                                                } else {Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();} // Exception message will be returned by Firebase in case of an error.
+                                            }
+                                        });}
+                                    else {Toast.makeText(RegisterActivity.this, "Student Number already in use.", Toast.LENGTH_LONG).show();}
+                                    }
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                    System.out.println("The read failed: " + databaseError.getCode());
+                                }
+                            });
+
                     } else {Toast.makeText(RegisterActivity.this, "Passwords do not match.", Toast.LENGTH_LONG).show();}
                 } else {Toast.makeText(RegisterActivity.this, "Please fill in all fields.", Toast.LENGTH_SHORT).show();}
             }
